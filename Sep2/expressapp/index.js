241878f5485dccebe2853a3fb4f40e93958d46b7
Eklavya <Piyush.Eklavya@team.telstra.com>
2020-09-02 00:10:43 +0530

    cloning express app

--- Sep2/expressapp/index.js
+++ Sep2/expressapp/index.js
@@ -1,1 +1,7 @@
-const app = require('express');
+const express = require('express');
+const app = express();
+
+app.use('/', (req,res) => {
+    console.log('main:index.js app.use(/) ')
+    res.write('main:index.js app.use(/) ');
+})
