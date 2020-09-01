97e9cb8aa51291b6b9a9a9f5cab031ed766e01a5
Eklavya <Piyush.Eklavya@team.telstra.com>
2020-09-02 00:16:19 +0530

    setUp express to run on port 3000

--- Sep2/expressapp/index.js
+++ Sep2/expressapp/index.js
@@ -4,4 +4,12 @@ const app = express();
 app.use('/', (req,res) => {
     console.log('main:index.js app.use(/) ')
     res.write('main:index.js app.use(/) ');
+    res.end('\n\nEOF res.end()')
+})
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log(`server started at http://localhost:${PORT}/`)
 })
