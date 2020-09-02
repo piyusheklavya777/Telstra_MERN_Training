931b28164ffa5963c48d4a06220147e168451425
Eklavya <Piyush.Eklavya@team.telstra.com>
2020-09-02 14:51:58 +0530

    organizeed mongoCommands

--- Sep2/mongoCommands.js
+++ Sep2/mongoCommands.js
@@ -1,28 +1,33 @@
-this is run in command prompt at any location. I did in Sep2 folder.
-
+//Create a data/db location. In the folder that contains data, cmd and
+//mongod --dbpath "./data/db".
+//then in any place (maybe your work directory), do these :
 show dbs
 
 use persons
 
+//making single enty
 db.person.insertOne({ name: "John",
      age:32
 })
+
+//replacing an entry
+db.person.replaceOne({name:"Johnathan"},{name:"Johnny"})
 
 //making relationships b/w objects
 db.person.insertOne({ name: "Jane",
      age:28,
     spouse :  ObjectId("5f4f39c8b1a2ac66397f41e0") })
-//
-db.person.find();
-db.person.find().pretty(); //displays better
+//finding an entry
+db.person.findOne({name:"John"});
+db.person.findOne({name:"John"}).pretty(); //displays better
 
-db.person.findOne({name:"John"})
-
-db.person.insertOne({object to insert})
+//making multiple entries
 db.person.insertMany()
+
+//deleting an entry
 db.person.deleteOne()
 db.person.deleteMany()
-
+//end
 
 //EMBEDDED DOCUMENTS
 
@@ -34,14 +39,9 @@ db.persons.insertOne({
         molar: 2
     }
 })
+//end
 
-db.person.insertOne({
-    name: 'Jill',
-    age: 4,
-    favoriteToys : ['toy-robo', 'hotwheels'] 
-})
-
-//Change John's Name : UpdateOne()
+////Change / Update an entry : UpdateOne()
 filterDoc2 = {name: 'John'}
 updateDoc2 = {
     $set: {
@@ -50,6 +50,18 @@ updateDoc2 = {
 };
 db.person.updateOne(filterDoc2, updateDoc2 )
 
+db.person.updateOne({ //when an embedded entry has to be updated 
+                      // put "" around it as . in numTeeth.milk will be
+                      //treated as an operator
+    "_id" : ObjectId("5f4f3e49b1a2ac66397f41e4")
+},{
+    $set: {
+        "numTeeth.milk" : 5  // since . is there so the key has to be in ""
+    }
+})
+//end
+
+///////////////////////////////////////////////////////////////////
 
 db.person.updateOne({
     "_id" : ObjectId("5f4f3e49b1a2ac66397f41e4")
@@ -58,3 +70,6 @@ db.person.updateOne({
         "numTeeth.milk" : 5  // since . is there so the key has to be in ""
     }
 })
+
+//replacing an entry
+db.person.replaceOne({name:"Johnathan"},{name:"Johnny"})
